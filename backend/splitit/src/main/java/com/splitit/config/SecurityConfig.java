fc3ea0c678bdd3dc96fd161821661286144d1913
pablobas2 <pablo.bas@alumnos.upm.es>
2025-03-30 20:28:08 +0200

    Actualización seguridad

--- backend/splitit/src/main/java/com/splitit/config/SecurityConfig.java
+++ backend/splitit/src/main/java/com/splitit/config/SecurityConfig.java
@@ -3,23 +3,20 @@ package com.splitit.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable()) // Desactiva CSRF para pruebas
+            .csrf(csrf -> csrf.disable()) // Desactivar CSRF solo para desarrollo
             .authorizeHttpRequests(auth -> auth
                 .anyRequest().permitAll()
             )
-            .httpBasic(Customizer.withDefaults());
-
+            .httpBasic(Customizer.withDefaults()); // Habilita autenticación básica si se desea
         return http.build();
     }
 }
