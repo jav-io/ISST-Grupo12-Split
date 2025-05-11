18f6ecd2998ac8069e7d8cdb70427ecbace6adab
pablobas2 <pablo.bas@alumnos.upm.es>
2025-05-11 19:41:51 +0200

    Mis avances locales antes del pull

--- backend/splitit/target/classes/schema.sql
+++
@@ -1,5 +0,0 @@
--- Tabla de usuarios con roles
-ALTER TABLE usuario ADD COLUMN IF NOT EXISTS roles text[];
-
--- Actualizar usuarios existentes con rol por defecto
-UPDATE usuario SET roles = ARRAY['ROLE_USER'] WHERE roles IS NULL; 
